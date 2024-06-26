2342ba2b2d16eed1a424c8aec5461b3a7f757717
Lucas-Dev15 <juan.lucas.dev15@gmail.com>
2024-06-26 16:56:31 +0200

    FINI

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-const body = document.querySelector("body");
-const navBar = document.querySelector("nav");
+const sections = document.querySelectorAll('section');
+const nav = document.querySelector('nav');
+const btnBurger = document.getElementById('burger-menu');
+const navigation = document.querySelector('.navigation');
+const links = document.querySelectorAll('.navigation a');
 
-body.addEventListener("scroll", () => {
-    navBar.classList.add("active");
-})
+btnBurger.addEventListener("click", () => {
+    navigation.classList.toggle('active');
+    btnBurger.classList.toggle('bx-x');
+});
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        navigation.classList.remove('active');
+        btnBurger.classList.remove('bx-x');
+    });
+});
+
+window.addEventListener('scroll', () => {
+    navigation.classList.remove('active');
+    btnBurger.classList.remove('bx-x');
+});
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 0) {
+        nav.classList.toggle('active');
+    }
+});
